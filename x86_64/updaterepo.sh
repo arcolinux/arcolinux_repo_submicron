17d4d9ba353180ddc959037c6819e1abc683b246
Erik Dubois <erik.dubois@gmail.com>
2018-10-03 19:29:52 +0200

    update

--- x86_64/updaterepo.sh
+++ x86_64/updaterepo.sh
@@ -4,9 +4,9 @@ rm arcolinux_repo*
 
 
 echo "repo-add"
-repo-add -s -n arcolinux_repo.db.tar.gz *.pkg.tar.xz
+repo-add -s -n arcolinux_repo-submicron.db.tar.gz *.pkg.tar.xz
 #sleep 8
-cp -f arcolinux_repo.db.tar.gz arcolinux_repo.db
+cp -f arcolinux_repo_submicron.db.tar.gz arcolinux_repo_submicron.db
 
 echo "####################################"
 echo "Repo Updated!!"
