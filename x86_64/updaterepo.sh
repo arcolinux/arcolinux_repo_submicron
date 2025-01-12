ed8c9699a4ea49ef73c9866eb82d3804f1c9035e
arcolinuxz <arcolinuxinfo@gmail.com>
2025-01-12 17:46:10 +0100

    update

--- x86_64/updaterepo.sh
+++ x86_64/updaterepo.sh
@@ -2,12 +2,22 @@
 
 rm arcolinux_repo*
 
-
 echo "repo-add"
-repo-add -s -n arcolinux_repo_submicron.db.tar.gz *.pkg.tar.xz
-repo-add -s -n arcolinux_repo_submicron.db.tar.gz *.pkg.tar.zst
-sleep 5
-cp -f arcolinux_repo_submicron.db.tar.gz arcolinux_repo_submicron.db
+repo-add -s -n -R arcolinux_repo_submicron.db.tar.gz *.pkg.tar.xz
+
+sleep 1
+
+rm arcolinux_repo_submicron.db
+rm arcolinux_repo_submicron.db.sig
+
+rm arcolinux_repo_submicron.files
+rm arcolinux_repo_submicron.files.sig
+
+mv arcolinux_repo_submicron.db.tar.gz arcolinux_repo_submicron.db
+mv arcolinux_repo_submicron.db.tar.gz.sig arcolinux_repo_submicron.db.sig
+
+mv arcolinux_repo_submicron.files.tar.gz arcolinux_repo_submicron.files
+mv arcolinux_repo_submicron.files.tar.gz.sig arcolinux_repo_submicron.files.sig
 
 echo "####################################"
 echo "Repo Updated!!"
